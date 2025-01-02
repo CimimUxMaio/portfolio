c0ea385031f3ea1ddc93bbb107def75a5001182f
CimimUxMaio <juanbiabdon@gmail.com>
2025-01-02 12:24:01 -0300

    Added script for certificates generation with certbot + added config for automatic cert renewal

--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -1,4 +1,3 @@
 FROM nginx:alpine
 COPY ./nginx.conf /etc/nginx/conf.d/default.conf
-COPY /ssl /ssl
 CMD ["nginx", "-g", "daemon off;"]
--- ssl/gen.sh
+++ ssl/gen.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-set -e  # Exit on error
+set -e
 
-rm -f *.pem
-echo "Removed all existing .pem files."
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "Generating CA's private key and self-signed certificate..."
-openssl req -x509 -newkey rsa:4096 -days 365 -keyout ca-key.pem -out ca-cert.pem
-echo "ca-key.pem and ca-cert.pem generated."
-
-echo "Generating server's private key and certificate signing request (CSR)..."
-openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem
-echo "server-key.pem and server-req.pem generated."
-
-echo "Signing server's CSR with CA's private key..."
-openssl x509 -req -in server-req.pem -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem
-echo "server-cert.pem generated."
-
-echo "Verifying server's certificate..."
-openssl verify -CAfile ca-cert.pem server-cert.pem
+docker run --rm -it -p 80:80 -v "$SCRIPT_DIR:/etc/letsencrypt" -v "$SCRIPT_DIR:/var/lib/letsencrypt" certbot/certbot certonly --standalone -m juanbiabdon@gmail.com --agree-tos -d jbabdon.dev -d www.jbabdon.dev
